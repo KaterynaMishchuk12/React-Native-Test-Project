6c80f72cb57c81653ee334eeed4f3de4fcb677b1
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2024-08-06 13:29:31 +0300

    registrScreen is made without svg

--- React-Native-Test-Project/Screens/RegistrationScreen.jsx
+++ React-Native-Test-Project/Screens/RegistrationScreen.jsx
@@ -11,8 +11,10 @@ import {
   Keyboard,
 } from "react-native";
 
+import AddIcon from "../assets/images/add.svg";
+
 export function RegistrationScreen() {
-  const [text, setText] = useState("");
+  const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordIsHidden, setPasswordIsHidden] = useState(true);
@@ -36,46 +38,54 @@ export function RegistrationScreen() {
             flex: 1,
           }}
         >
-          <Text style={styles.title}>Реєстрація</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="Логін"
-              value={text}
-              onChangeText={(text) => setText(text)}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Адреса електронної пошти"
-              textContentType="emailAddress"
-              value={email}
-              onChangeText={(value) => setEmail(value)}
-            />
-            <View style={styles.passwordContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder="Пароль"
-                textContentType="password"
-                secureTextEntry={passwordIsHidden}
-                value={password}
-                onChangeText={(value) => setPassword(value)}
-              />
-              <TouchableOpacity
-                style={styles.passwordHideBtn}
-                onPress={() => setPasswordIsHidden(!passwordIsHidden)}
-              >
-                <Text style={styles.passwordHideText}>
-                  {passwordIsHidden ? "Показати" : "Приховати"}
-                </Text>
+          <View style={styles.infoWrapper}>
+            <View style={styles.avatarContainer}></View>
+            <TouchableOpacity style={styles.addIcon}>
+              {/* <AddIcon /> */}
+            </TouchableOpacity>
+            <View style={styles.infoBox}>
+              <Text style={styles.title}>Реєстрація</Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Логін"
+                  value={login}
+                  onChangeText={(login) => setLogin(login)}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Адреса електронної пошти"
+                  textContentType="emailAddress"
+                  value={email}
+                  onChangeText={(value) => setEmail(value)}
+                />
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Пароль"
+                    textContentType="password"
+                    secureTextEntry={passwordIsHidden}
+                    value={password}
+                    onChangeText={(value) => setPassword(value)}
+                  />
+                  <TouchableOpacity
+                    style={styles.passwordHideBtn}
+                    onPress={() => setPasswordIsHidden(!passwordIsHidden)}
+                  >
+                    <Text style={styles.passwordHideText}>
+                      {passwordIsHidden ? "Показати" : "Приховати"}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.registerText}>Зареєструватися</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.toLogin}>
+                <Text>Вже є акаунт? Увійти</Text>
               </TouchableOpacity>
             </View>
           </View>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.registerText}>Зареєструватися</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.toLogin}>
-            <Text>Вже є акаунт? Увійти</Text>
-          </TouchableOpacity>
         </ImageBackground>
       </View>
     </TouchableWithoutFeedback>
@@ -87,12 +97,34 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
   },
+  infoWrapper: {
+    // flex: 1,
+    alignItems: "center",
+    // justifyContent: "flex-end",
+    backgroundColor: "#FFFFFF",
+    // height: 550,
+    paddingHorizontal: 16,
+
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    width: "100%",
+  },
+  avatarContainer: {
+    position: "absolute",
+    top: -60,
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+  infoBox: { width: "100%" },
   title: {
     fontSize: 30,
     fontWeight: "bold",
     lineHeight: 35,
     color: "black",
     textAlign: "center",
+    marginTop: 92,
     marginBottom: 32,
   },
   button: {
@@ -103,6 +135,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 100,
     marginBottom: 16,
+    width: "100%",
   },
   registerText: {
     color: "white",
@@ -111,6 +144,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     gap: 16,
     marginBottom: 43,
+    width: "100%",
   },
   input: {
     height: 40,
@@ -118,13 +152,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingHorizontal: 10,
     borderRadius: 8,
+    width: "100%",
   },
   passwordContainer: {
     position: "relative",
   },
   passwordHideBtn: {
     position: "absolute",
-    top: 15,
+    top: 10,
     right: 12,
   },
   passwordHideText: {
